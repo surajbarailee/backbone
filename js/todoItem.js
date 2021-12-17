a6ba73dfddd55466cbfe298d8dd61af07f8d435d
Suraj Baraili <baraileesuraj@gmail.com>
2021-12-17 21:36:10 +0545

    simple toggle

--- js/todoItem.js
+++ js/todoItem.js
@@ -1,12 +1,17 @@
 var TodoItem = Backbone.Model.extend(
 {
+    defaults:{
+        isCompleted:false
+    },
     validate: function(attrs)
     {
         if (!attrs.description)
-        {
-            return "DEscription is required";
-        }
+            return "Description is required";
+    },
+    toggle: function(){
+        this.set('isCompleted', !this.get('isCompleted'));
     }
+
 }
 
 
--- js/todoItemView.js
+++ js/todoItemView.js
@@ -2,11 +2,22 @@ var TodoItemView = Backbone.View.extend({
     tagName: 'li',
     initialize: function(options) {
         if(!(options && options.model))
-        throw new Error("TodoItemView requires a model");
+            throw new Error("TodoItemView requires a model");
+        this.model.on('change',this.render,this)
+    },
+
+    events:{
+        'click #toggle': 'onClickToggle'
+    },
+    onClickToggle: function(){
+        this.model.toggle()
     },
 
     render:function(){
-        this.$el.html(this.model.escape('description'));
+        this.$el.toggleClass('completed', this.model.get('isCompleted'))
+
+        var checked = this.model.get('isCompleted') ? 'checked' : ''
+        this.$el.html("<input id='toggle' type='checkbox' " + checked + "></input>"+ this.model.escape('description'));
         return this;
     }
 });
