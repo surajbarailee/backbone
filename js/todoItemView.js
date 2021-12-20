bee32198080d513ddf403ef4774b0bf71bc3a03b
surajbarailee <baraileesuraj@gmail.com>
2021-12-20 22:30:16 +0545

    templating

--- js/todoItemView.js
+++ js/todoItemView.js
@@ -23,8 +23,9 @@ var TodoItemView = Backbone.View.extend({
 
         this.$el.attr('id',this.model.id);
         this.$el.toggleClass('completed', this.model.get('completed'))
-        var checked = this.model.get('completed') ? 'checked' : ''
-        this.$el.html("<input id='toggle' type='checkbox' " + checked + "></input>"+ this.model.escape('title') + "<button id='delete'>delete</button>");
+        var template = $("#todoItemTemplate").html();
+        var html = Mustache.render(template,this.model.toJSON());
+        this.$el.html(html);
         return this;
     }
 });
